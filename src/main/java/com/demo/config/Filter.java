a3dd24cba3ce1896375c665a21259d2e087b6e7d
jianghaotian <jiang.haotian@trs.com.cn>
2019-01-18 10:47:58 +0800

    init

---
+++ src/main/java/com/demo/config/Filter.java
@@ -0,0 +1,32 @@
+package com.demo.config;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * Title:
+ * Description:
+ * Copyright: 2019 北京拓尔思信息技术股份有限公司 版权所有.保留所有权
+ * Company:北京拓尔思信息技术股份有限公司(TRS)
+ * Project: JavaEE
+ * Author: jianghaotian
+ * Create Time:2019/1/18 10:38
+ */
+@WebFilter(filterName = "Filter")
+public class Filter implements javax.servlet.Filter {
+    @Override
+    public void destroy() {
+        System.out.println("destroy");
+    }
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        System.out.println("doFilter");
+        chain.doFilter(req, resp);
+    }
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+        System.out.println("init");
+    }
+
+}
---
+++ src/main/java/com/demo/config/Listener.java
@@ -0,0 +1,82 @@
+package com.demo.config; /**
+ * Title:
+ * Description:
+ * Copyright: 2019 北京拓尔思信息技术股份有限公司 版权所有.保留所有权
+ * Company:北京拓尔思信息技术股份有限公司(TRS)
+ * Project: JavaEE
+ * Author: jianghaotian
+ * Create Time:2019/1/18 10:38
+ */
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+
+@WebListener()
+public class Listener implements ServletContextListener,
+        HttpSessionListener, HttpSessionAttributeListener {
+
+    // Public constructor is required by servlet spec
+    public Listener() {
+    }
+
+    // -------------------------------------------------------
+    // ServletContextListener implementation
+    // -------------------------------------------------------
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+      /* This method is called when the servlet context is
+         initialized(when the Web application is deployed). 
+         You can initialize servlet context related data here.
+      */
+        System.out.println("contextInitialized");
+    }
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+      /* This method is invoked when the Servlet Context 
+         (the Web application) is undeployed or 
+         Application Server shuts down.
+      */
+        System.out.println("contextDestroyed");
+    }
+
+    // -------------------------------------------------------
+    // HttpSessionListener implementation
+    // -------------------------------------------------------
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+      /* Session is created. */
+        System.out.println("sessionCreated");
+    }
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+      /* Session is destroyed. */
+        System.out.println("sessionDestroyed");
+    }
+
+    // -------------------------------------------------------
+    // HttpSessionAttributeListener implementation
+    // -------------------------------------------------------
+    @Override
+    public void attributeAdded(HttpSessionBindingEvent sbe) {
+      /* This method is called when an attribute 
+         is added to a session.
+      */
+    }
+    @Override
+    public void attributeRemoved(HttpSessionBindingEvent sbe) {
+      /* This method is called when an attribute
+         is removed from a session.
+      */
+    }
+    @Override
+    public void attributeReplaced(HttpSessionBindingEvent sbe) {
+      /* This method is invoked when an attibute
+         is replaced in a session.
+      */
+    }
+}
